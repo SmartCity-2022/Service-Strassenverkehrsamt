e535d6ac1e746bb7cbe5430b17c9fee14e1b84fe
KevFischer <kevinf260800mail.com>
2022-05-03 19:43:47 +0200

    Added various views

--- SmartCity/api/urls.py
+++ SmartCity/api/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
+from rest_framework import routers
 from . import views
 
+
 urlpatterns = [
-     path("", views.getData),
+     path("bill", views.get_all_bills),
+     path("bill/create", views.add_bill),
+     path("license", views.get_all_licenses),
+     path("license/create", views.add_license),
+     path("licenserequest", views.get_all_license_requests),
+     path("licenserequest/create", views.add_license_request),
+     path("vehicle", views.get_all_vehicles),
+     path("vehicle/create", views.add_vehicle),
+     path("registerrequest", views.get_all_register_requests),
+     path("registerrequest/create", views.add_register_request),
+     path("penalty", views.get_all_penaltys),
+     path("penalty/create", views.add_penalty),
 ]
--- SmartCity/api/views.py
+++ SmartCity/api/views.py
@@ -1,7 +1,93 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from stva.models import *
+from .serializers import *
 
 @api_view(["GET"])
-def getData(request):
-    person = {"name": "Test"}
-    return Response(person)
+def get_all_bills(request):
+    bills = Bill.objects.all()
+    serializer = BillSerializer(bills, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_bill(request):
+    serializer = BillSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_all_licenses(request):
+    licenses = License.objects.all()
+    serializer = LicenseSerializer(licenses, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_license(request):
+    serializer = LicenseSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_all_license_requests(request):
+    requests = Licenserequest.objects.all()
+    serializer = LicenseRequestSerializer(requests, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_license_request(request):
+    serializer = LicenseRequestSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_all_penaltys(request):
+    penaltys = Penalty.objects.all()
+    serializer = PenaltySerializer(penaltys, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_penalty(request):
+    serializer = PenaltySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_all_register_requests(request):
+    requests = Registerrequest.objects.all()
+    serializer = RegisterRequestSerializer(requests, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_register_request(request):
+    serializer = RegisterRequestSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_all_vehicles(request):
+    vehicles = Vehicle.objects.all()
+    serializer = VehicleSerializer(vehicles, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_vehicle(request):
+    serializer = VehicleSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)

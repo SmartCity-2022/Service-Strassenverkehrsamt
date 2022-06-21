938f5f99c1d022cd647c52ef1463ed1aa5c70f38
KevFischer <kevinf260800mail.com>
2022-06-21 22:43:21 +0200

    Potential auth fix

--- SmartCity/SmartCity/settings.py
+++ SmartCity/SmartCity/settings.py
@@ -141,7 +141,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://strassenverkehrsamt-backend.smartcity.svc.cluster.local'
+    'http://strassenverkehrsamt-backend.smartcity.svc.cluster.local',
+    'https://strassenverkehrsamt-backend.smartcity.svc.cluster.local'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
